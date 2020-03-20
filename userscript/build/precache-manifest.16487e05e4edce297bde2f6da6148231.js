aa2d154593b2ca93027bace9ed756fe90c4bd60c
jamesona <jamesonaranda@gmail.com>
2020-03-20 08:13:51 -0600

    I guess it builds one level up...

---
+++ userscript/build/precache-manifest.16487e05e4edce297bde2f6da6148231.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128a2d0f35cf8a5cdb1c76f552e68f1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de257272786a9c610618",
+    "url": "/static/js/main.js"
+  }
+]);
