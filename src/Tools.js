a53fba54663f94e735c1acf937c36ec0ed1a007c
jamesona <jamesonaranda@gmail.com>
2020-03-20 08:08:37 -0600

    buildable

---
+++ src/Tools.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import './Tools.css';
+
+// const toolbarStyle = {
+// 	backgroundColor: 'black',
+// 	display: 'flex',
+// 	left: '10px',
+// 	position: 'fixed',
+// 	top: '10px',
+// 	width: '100%'
+// }
+
+function Tools() {
+  return (
+    <div className="torn-tools">
+      <header className="header">
+        <p>
+          Edit <code>src/Tools.js</code> and save. Then, refresh the page.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Tools;
